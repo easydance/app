548812670affc28b4134c7db0e38ceaa188efc01
antobru-davinci <a.bruno@davinci.care>
2023-11-20 22:22:32 +0100

    feat: order-detail added

--- src/app/pages/users/pages/full-immersion/pages/order-detail/order-detail.page.ts
+++ src/app/pages/users/pages/full-immersion/pages/order-detail/order-detail.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { NavController } from '@ionic/angular';
+import { TableOrderBaseDto, TableOrderService } from 'src/app/apis';
+import { AuthManagerService } from 'src/app/services/auth-manager.service';
 
 @Component({
   selector: 'order-detail',
@@ -7,9 +11,65 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrderDetailPage implements OnInit {
 
-  constructor() { }
+  public order?: TableOrderBaseDto;
+
+  constructor(
+    private navCtrl: NavController,
+    private ordersService: TableOrderService,
+    private authManager: AuthManagerService,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit() {
+    this.route.params.subscribe(res => {
+      if (res['id']) {
+        this.ordersService.findOne(res['id'], undefined, 'table, rows..priceListItem.product').subscribe(res => {
+          this.order = res.data;
+        });
+      }
+    });
   }
 
+  goBack() {
+    this.navCtrl.back();
+  }
+
+
+  getStatusLabel(status: string) {
+    switch (status) {
+      case 'CREATED':
+      case 'IN_PROGRESS':
+        return 'Sta arrivando!';
+
+      case 'COMPLETED':
+      case 'PROCESSED':
+      case 'DELIVERED':
+        return 'Consegnato';
+
+      case 'CANCELED':
+        return 'ANNULLATO';
+
+      default:
+        return '-';
+    }
+  }
+
+  getStatusColor(status: string) {
+    switch (status) {
+      case 'CREATED':
+      case 'IN_PROGRESS':
+        return 'warning';
+
+      case 'COMPLETED':
+      case 'PROCESSED':
+      case 'DELIVERED':
+        return 'success';
+
+      case 'CANCELED':
+        return 'danger';
+
+      default:
+        return '-';
+    }
+  }
 }
