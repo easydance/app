adfeaa4558f28e37e97c0afd2af1bc79dfab4e53
antobru-davinci <a.bruno@davinci.care>
2024-04-15 11:27:31 +0200

    fix updater

--- src/app/services/version-updater.service.ts
+++ src/app/services/version-updater.service.ts
@@ -11,79 +11,26 @@ export class VersionUpdaterService {
   constructor() { }
 
   static async init() {
-    const result = await CapacitorUpdater.notifyAppReady();
-    console.log({ method: 'CapacitorUpdater.notifyAppReady()', value: result });
+    await CapacitorUpdater.notifyAppReady();
+    const { bundle: currentBundle } = await CapacitorUpdater.current();
+    this.printBundleInfo(currentBundle, 'CURRENT BUNDLE');
 
-    // await this.removeAllBundles();
+    if (currentBundle.version == window.EASY_KEYS?.['LAST_HOTFIX_VERSION']) {
+      console.log("HotFixVersion same to now!");
+      return;
+    }
+
     let data: BundleInfo | null = await CapacitorUpdater.download({
       url: window.EASY_KEYS?.['LAST_HOTFIX_URL'] || '',
       version: window.EASY_KEYS?.['LAST_HOTFIX_VERSION'] || '0.0.0',
 
     });
-    console.log({
-      method: 'CapacitorUpdater.download(...)',
-      params: [{
-        url: window.EASY_KEYS?.['LAST_HOTFIX_URL'] || '',
-        version: window.EASY_KEYS?.['LAST_HOTFIX_VERSION'] || '0.0.0',
-
-      }],
-      value: data
-    });
-
     this.printBundleInfo(data, 'CURRENT HOTFIX');
-    const { bundle: currentBundle } = await CapacitorUpdater.current();
-    this.printBundleInfo(currentBundle, 'CURRENT BUNDLE');
-    console.log({
-      method: 'CapacitorUpdater.current()',
-      value: data
-    });
 
-    console.log({
-      method: 'result.bundle.version != currentBundle.version && data',
-      params: {
-        result,
-        currentBundle,
-        data
-      },
-      value: data.version != currentBundle.version && data
-    });
-    // Do the switch when user leave app
-    if (data.version != currentBundle.version && data) {
+    if (data) {
       SplashScreen.show({ fadeOutDuration: 500 });
       await this.setVersion(data);
-      // try {
-      //   await CapacitorUpdater.set({ id: data.id });
-      // } catch (err) {
-      //   console.log(err);
-      //   SplashScreen.hide({ fadeOutDuration: 500 }); // in case the set fail, otherwise the new app will have to hide it
-      // }
     }
-
-    // const result = await CapacitorUpdater.notifyAppReady();
-    // let data: BundleInfo | null = null;
-    // App.addListener('appStateChange', async (state) => {
-    //   if (state.isActive) {
-    //     console.log('App is active');
-    //     // Do the download during user active app time to prevent failed download
-    //     data = await CapacitorUpdater.download({
-    //       url: window.EASY_KEYS?.['LAST_HOTFIX_URL'] || '',
-    //       version: window.EASY_KEYS?.['LAST_HOTFIX_VERSION'] || '0.0.0',
-    //     });
-    //     this.printBundleInfo(data);
-    //   }
-    //   if (!state.isActive && data) {
-    //     console.log('App is background');
-    //     // Do the switch when user leave app
-    //     this.printBundleInfo(data, 'LOADED BUNDLE');
-    //     SplashScreen.show();
-    //     try {
-    //       await CapacitorUpdater.set({ id: data.id });
-    //     } catch (err) {
-    //       console.log(err);
-    //       SplashScreen.hide(); // in case the set fail, otherwise the new app will have to hide it
-    //     }
-    //   }
-    // });
   }
 
   static async removeAllBundles(excludeVersions: string[] = []) {
